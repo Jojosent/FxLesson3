1123eea7d5f9586e0ce105b66f9f04fc34e6f63a
Jojosent <aldabaizhomart@gmail.com>
2022-09-30 12:06:00 +0600

    javaFx Les4

---
+++ src/main/java/com/example/fxjavaproject/Dash_Controller.java
@@ -0,0 +1,19 @@
+package com.example.fxjavaproject;
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Dash_Controller  {
+
+}
